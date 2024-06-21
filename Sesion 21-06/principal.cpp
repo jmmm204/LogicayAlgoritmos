84ab2cd4f8ef13d75aaf2918452da6c2402cc0b5
jmmm204 <164056385+jmmm204@users.noreply.github.com>
2024-06-21 08:40:01 -0600

    Add files via upload

---
+++ Sesion 21-06/principal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <locale.h>
+#include "daoCiudad.cpp"
+
+using namespace std;
+
+int main()
+{
+    /* code */
+    setlocale(LC_ALL, "spanish");
+    principal();
+    return 0;
+}
